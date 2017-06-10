12822233885a0f3fd46bd722338af272e728985a
c0desBym3ta <kalimetasploiter@gmail.com>
2017-06-10 18:21:16 +0200

    Makine our object adapter but some problems remaining.

--- Adapter/TextShapeAdapter.cpp
+++ Adapter/TextShapeAdapter.cpp
@@ -1,25 +1,14 @@
 #include "TextShapeAdapter.h"
 
-TextShapeAdapter::TextShapeAdapter(const Text &adaptee) 
-	: Text(adaptee.getText(), adaptee.getFontSize()){
+void TextShapeAdapter::draw() {
+    // ...
+    adaptee->print();
+    // ...
 
 }
 
-TextShapeAdapter::TextShapeAdapter(string t, int fontSize) : Text(t, fontSize){
-
+void TextShapeAdapter::resize(float newSize) {
+    int textNewSize = static_cast<int>(newSize * adaptee->getFontSize());
+    adaptee->resize( textNewSize );
 }
 
-TextShapeAdapter::~TextShapeAdapter(){
-
-}
-
-void TextShapeAdapter::draw(){
-	//.....
-	Text::print();
-	//.....
-}
-
-void TextShapeAdapter::resize(float newSize){
-	int textNewSize = static_cast<int>(newSize); //Casting from one float type to int.
-	Text::resize(textNewSize);
-}
--- Adapter/TextShapeAdapter.h
+++ Adapter/TextShapeAdapter.h
@@ -5,23 +5,28 @@
 #include <string>
 #include "Text.h"
 #include "Shape.h"
+using namespace std;
+
 
 //This will be our adapter.
+//Making soe changes and save our previous class adapter source code to anothes txt file.
+//We are going to implement our object adapter.
 
-class TextShapeAdapter : public Shape, private Text{
+class TextShapeAdapter : public Shape {
 public:
-	TextShapeAdapter(const Text &adaptee);
-	TextShapeAdapter(string t, int fontSize);
-	virtual ~TextShapeAdapter();
+    TextShapeAdapter(Text* adText) :
+            adaptee(adText) {}
+    TextShapeAdapter(std::string t, int fontSize) {
+        adaptee = new Text(t, fontSize);
+    }
+    virtual ~TextShapeAdapter() {}
 
-	//Overriding our methods.
-	virtual void draw() override;
-	virtual void resize(float newSize) override;
+    virtual void draw() override;
+    virtual void resize(float newSize) override;
 
 private:
-
+    Text* adaptee;
 };
-
 
 
 #endif 
