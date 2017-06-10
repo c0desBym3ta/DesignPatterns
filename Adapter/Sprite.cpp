c9f094ee5ba21fec8d2ac7907b137879393bcc0c
c0desBym3ta <kalimetasploiter@gmail.com>
2017-06-10 15:04:46 +0200

    Implementing the Sprite class but some error with override occurs.

--- Adapter/Sprite.cpp
+++ Adapter/Sprite.cpp
@@ -1,2 +1,18 @@
 #include "Sprite.h"
 
+Sprite::Sprite(string fileN, int w, int h) : width(w), height(h), fileName(fileN){
+
+}
+
+Sprite::~Sprite(){
+
+}
+
+void Sprite::draw(){
+	cout << "Draw sprite: " << fileName << " -w: " << width << " -h: " << height << endl;
+}
+
+void Sprite::resize(float newSize){
+	width *= newSize;
+	height *= newSize;
+}
--- Adapter/Sprite.h
+++ Adapter/Sprite.h
@@ -7,9 +7,29 @@ using namespace std;
 
 class Sprite {
 public:
+	//Againg our explicit constructor.
+	explicit Sprite(string fileN, int w = 100, int h = 200);
+	//And our virtual destructor
+	virtual ~Sprite();
+	
+	//Time to set and then implement the methods.
+	virtual void draw() override;
+	virtual void resize(float newSize) override;
+	
+	//Making setters and getter to take access to the private variables.
+	void setWidth(int width){Sprite::width = width;}
+	int getWidht() const { return width;}
+	
+	void setHeight(int height) { Sprite::height = height;}
+	int getHeight() const { return height;}
 
-protected:
+	void setFileName(const string &fileName){Sprite::fileName = fileName;}
+	const string &getFileName() const { return fileName;}
 
+private:
+	//Two unique variables for this derived class.
+	int width, height;
+	string fileName;
 };
 
 
