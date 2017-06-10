85e9cf2e69f4f869ecb3e6b4d233f0b8709b3243
c0desBym3ta <kalimetasploiter@gmail.com>
2017-06-10 17:24:38 +0200

    Implementing the Text class.

--- Adapter/Text.cpp
+++ Adapter/Text.cpp
@@ -1,2 +1,17 @@
 #include "Text.h"
 
+Text::Text(string t, int s) : text(t), fontSize(s){
+
+}
+
+Text::~Text(){
+
+}
+
+void Text::print(){
+	cout << "Font size: " << fontSize << " - Text: " << text << endl;
+}
+
+void Text::resize(int newSize){
+	fontSize += newSize;
+}
--- Adapter/Text.h
+++ Adapter/Text.h
@@ -1,14 +1,31 @@
 #ifndef TEXT_H
 #define TEXT_H
 
+//Here i will make comments when its necessary.
+
 #include <iostream>
 #include <string>
+using namespace std;
 
 class Text {
 public:
+	explicit Text(string t = "", int s = 8);
+	virtual ~Text();
+
+	virtual void print();
+	virtual void resize(int newSize);
+
+	//Getters/Setters methods.
+	void setText(const string &text){Text::text = text;}
+	const string &getText() const{return text;}	
+	
+	void setFontSize(int fontSize){Text::fontSize = fontSize;}
+	int getFontSize() const { return fontSize;}
 
 protected:
-
+	//Unique Attributes
+	string text;
+	int fontSize;
 };
 
 
